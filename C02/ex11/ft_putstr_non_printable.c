c1a68ec51927349e37a1fbc5e2d714c744e717eb
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2025-08-14 14:32:49 +0900

    modification before push (for norminette)

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: blanglai <blanglai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 09:41:22 by blanglai          #+#    #+#             */
-/*   Updated: 2025/08/14 13:17:12 by blanglai         ###   ########.fr       */
+/*   Updated: 2025/08/14 14:28:44 by blanglai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,26 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void ft_convert(unsigned char c)
+void	ft_convert(unsigned char c)
 {
-	char *hex = "0123456789abcdef";
+	char	hex[16];
+
+	hex[0] = '0';
+	hex[1] = '1';
+	hex[2] = '2';
+	hex[3] = '3';
+	hex[4] = '4';
+	hex[5] = '5';
+	hex[6] = '6';
+	hex[7] = '7';
+	hex[8] = '8';
+	hex[9] = '9';
+	hex[10] = 'a';
+	hex[11] = 'b';
+	hex[12] = 'c';
+	hex[13] = 'd';
+	hex[14] = 'e';
+	hex[15] = 'f';
 	ft_putchar('\\');
 	ft_putchar(hex[c / 16]);
 	ft_putchar(hex[c % 16]);
@@ -41,12 +58,11 @@ void	ft_putstr_non_printable(char *str)
 			ft_convert(*p);
 		}
 		p++;
-
 	}
 }
 
-int	main(void)
-{
-	char a[] = "bonjo\rur je suis \nravie &de\a vOUS (rEncontrer)";
-	ft_putstr_non_printable(a);
-}
+// int	main(void)
+// {
+// 	chara[] = "bonjo\rur je suis \nravie &de\a vOUS (rEncontrer)";
+// 	ft_putstr_non_printable(a);
+// }

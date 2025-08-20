680e0c0c6d3ec799fe4efa2eddff0edee71ce2dd
Rouboufy <213021409+Rouboufy@users.noreply.github.com>
2025-08-20 17:47:46 +0900

    jg;kjh

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blanglai <blanglai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 15:31:15 by blanglai          #+#    #+#             */
+/*   Updated: 2025/08/20 16:10:59 by blanglai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb <= 0)
+		return (0);
+	result = 1;
+	while (nb > 1)
+	{
+		result = result * nb;
+		nb--;
+	}
+	return (result);
+}
+
+// int	main(void)
+// {
+// 	printf("%d", ft_iterative_factorial(1234567));
+// }
---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blanglai <blanglai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 15:43:22 by blanglai          #+#    #+#             */
+/*   Updated: 2025/08/20 15:58:12 by blanglai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	result = 0;
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+	{
+		return (1);
+	}
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
